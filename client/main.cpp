d7368a88ff11eec9f953f0632057ebf5461574be
aliakseis <aliaksei.sanko@gmail.com>
2019-11-29 15:04:08 +0300

    client stopping

--- client/main.cpp
+++ client/main.cpp
@@ -1,11 +1,37 @@
 #include "PublishSubscribeClient.h"
 
+#include <signal.h>
+
 #include <iostream>
+
+static std::unique_ptr<IPublishSubscribeClient> client;
+
+void signalHandler(int signo)
+{
+    if (client) {
+        client->TryCancel();
+    }
+}
+
+
+void setSignalHandler()
+{
+#ifdef _WIN32
+    signal(SIGINT, signalHandler);
+#else
+    struct sigaction sa;
+    sa.sa_handler = signalHandler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+#endif
+}
+
 
 int main()
 {
     auto lam = [](const PlainNotification& notification) {
         std::cout << notification.index << ' ' << notification.content << ',';
     };
-    auto client = MakePublishSubscribeClient("localhost:50051", "42", lam);
+    client = MakePublishSubscribeClient("localhost:50051", "42", lam);
 }
